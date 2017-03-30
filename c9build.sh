c6dc0ad06551e779b72b525d3b183ec07ec27d00
Mark Underseth <marku@s2technologies.com>
2017-03-30 20:33:19 +0000

    config change

--- c9build.sh
+++ c9build.sh
@@ -10,5 +10,5 @@ curl -s https://testspace-client.s3.amazonaws.com/testspace-linux-dev.tgz | sudo
 
 # Post (using "mark-s2-tec")
 export MARK_TOKEN=095d8acfaaf71b4a0f3767e41152620ebdda1b31
-testspace config url http://$MARK_TOKEN@mark.stridespace.com/box_CIs_standalone
-testspace test.xml space-1
+testspace config url http://$MARK_TOKEN@mark.stridespace.com/box_CIs_standalone/c9
+testspace test.xml 
