bc75ee62ac379758b7fa39a4e26062f461c65aea
Mark Underseth <marku@s2technologies.com>
2017-04-12 01:16:13 +0000

    more cleanup

--- c9build.sh
+++ c9build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Client install and configure (using "mark-s2-tec")
-mkdir -p $HOME/bin  
 curl -s https://testspace-client.s3.amazonaws.com/testspace-linux-dev.tgz | tar -zxvf- -C $HOME/bin
 testspace config url http://095d8acfaaf71b4a0f3767e41152620ebdda1b31:@mark.stridespace.com/box_CIs_standalone
 testspace -v
